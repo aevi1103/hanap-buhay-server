4976b2fe4d726ad0ba7d3074c81bfd2e6340ec3d
aevik <aevik@JABI>
2022-05-22 08:57:53 -0400

    fix: modify script

--- scripts.sql
+++ scripts.sql
@@ -5,5 +5,6 @@ create table users (
 	first_name varchar(100) not null,
 	last_name varchar(100) not null,
 	date_created timestamptz not null,
-	date_modified timestamptz
+	date_modified timestamptz,
+	password text not null
 )
